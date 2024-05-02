698f53bd676785c532d3b9cdf89053e60f834d80
Yujun Chen <steven.yujunchen@berkeley.edu>
2024-05-02 12:52:53 +0800

    edit ipyn

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mcqgenerator",
+    version="0.0.1",
+    author="steven chen",
+    author_email="labotrr@gmail.com",
+    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    packages=find_packages(),
+)
